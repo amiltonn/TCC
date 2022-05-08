40aa990a3b060ea3251ede2d8068135451ad90cd
Gabriel-sardinha <gabrielsardinhaferreira@gmail.com>
2022-05-08 12:12:52 -0300

    criando activity main

---
+++ app/src/main/java/com/tcc/zipzop/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tcc.zipzop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Hello ZipZop", Toast.LENGTH_SHORT).show();
+    }
+}
