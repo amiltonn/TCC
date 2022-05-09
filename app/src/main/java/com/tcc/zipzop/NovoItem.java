710851edab2896f6183f56acdfc99ba205934ad1
amilton.jesus <amilton.jesus@semprecub.com>
2022-05-09 15:13:40 -0300

    estruturas das telas

---
+++ app/src/main/java/com/tcc/zipzop/NovoItem.java
@@ -0,0 +1,33 @@
+package com.tcc.zipzop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NovoItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.Actionbar);
+        setContentView(R.layout.activity_novo_item);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Novo Item");
+
+    }
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+
+        switch (menuItem.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+}
