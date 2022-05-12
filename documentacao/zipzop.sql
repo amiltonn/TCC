695e94c3200ec1abcae6e2e945ace1b9411660b1
keybot <mail.keybot@gmail.com>
2022-05-12 08:42:38 -0300

    db v8 vrum vrum

--- documentacao/zipzop.sql
+++ documentacao/zipzop.sql
@@ -1,5 +1,6 @@
 -- DROP EM TUDO
 
+DROP VIEW IF EXISTS venda_item_view;
 
 DROP VIEW IF EXISTS caixa_item_aberto;
 
@@ -9,36 +10,32 @@ DROP VIEW IF EXISTS caixa_item_view;
 
 DROP VIEW IF EXISTS item_view;
 
-DROP TABLE IF EXISTS estoque_item;
-
-DROP TABLE IF EXISTS estoque;
-
-DROP TABLE IF EXISTS venda_item;
-
 DROP TABLE IF EXISTS caixa_item;
-
-DROP TABLE IF EXISTS venda;
 
 DROP TABLE IF EXISTS caixa;
 
-DROP TABLE IF EXISTS forma_pagamento;
+DROP TABLE IF EXISTS caixa_fundo;
 
-DROP TABLE IF EXISTS receita_item;
+DROP TABLE IF EXISTS venda;
+
+DROP TABLE IF EXISTS forma_pagamento;
 
 DROP TABLE IF EXISTS insumo;
 
-DROP TABLE IF EXISTS unidade_medida;
-
-DROP TABLE IF EXISTS status_item;
-
 DROP TABLE IF EXISTS item;
 
--- CREATES
+DROP TABLE IF EXISTS formula_insumo;
 
-CREATE TABLE IF NOT EXISTS status_item(
-	id INTEGER PRIMARY KEY NOT NULL,
-	nome TEXT NOT NULL UNIQUE
-);
+DROP TABLE IF EXISTS estoque;
+
+DROP TABLE IF EXISTS unidade_medida;
+
+DROP TABLE IF EXISTS estoque_item;
+
+DROP TABLE IF EXISTS venda_item;
+
+
+-- CREATES
 
 CREATE TABLE IF NOT EXISTS unidade_medida(
 	id INTEGER PRIMARY KEY NOT NULL,
@@ -52,7 +49,7 @@ CREATE TABLE IF NOT EXISTS forma_pagamento(
 
 CREATE TABLE IF NOT EXISTS estoque(
 	id INTEGER PRIMARY KEY NOT NULL,
-	data_alteracao DATETIME NOT NULL UNIQUE
+	data_alteracao DATETIME NOT NULL UNIQUE DEFAULT (datetime())
 );
 
 CREATE TABLE IF NOT EXISTS item(
@@ -61,49 +58,57 @@ CREATE TABLE IF NOT EXISTS item(
 	qtd INTEGER NOT NULL,
 	custo NUMERIC NOT NULL,
 	preco NUMERIC,
-	data_alteracao DATETIME NOT NULL,
-	item_antes_id INTEGER DEFAULT NULL,
-	status_item_id INTEGER NOT NULL,
+	ativo BOOLEAN NOT NULL DEFAULT true CHECK (ativo IN (false, true)),
+	atual BOOLEAN NOT NULL DEFAULT true CHECK (atual IN (false, true)),
+	data_alteracao DATETIME NOT NULL DEFAULT (datetime()),
+	item_antes_id INTEGER DEFAULT NULL,	
 	unidade_medida_id INTEGER NOT NULL,
-	FOREIGN KEY (status_item_id) REFERENCES status_item (id) ON UPDATE CASCADE,
-	FOREIGN KEY (unidade_medida_id) REFERENCES unidade_medida (id) ON UPDATE CASCADE
+	FOREIGN KEY (unidade_medida_id) REFERENCES unidade_medida (id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS estoque_item(
 	estoque_id INTEGER NOT NULL,
 	item_id INTEGER NOT NULL,
-	UNIQUE(estoque_id, item_id),
-	FOREIGN KEY (estoque_id) REFERENCES estoque (id) ON UPDATE CASCADE,
-	FOREIGN KEY (item_id) REFERENCES item (id) ON UPDATE CASCADE
-);
+	PRIMARY KEY (estoque_id, item_id),
+	FOREIGN KEY (estoque_id) REFERENCES estoque (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	FOREIGN KEY (item_id) REFERENCES item (id) ON UPDATE RESTRICT ON DELETE RESTRICT
+) WITHOUT ROWID;
 
 CREATE TABLE IF NOT EXISTS caixa(
 	id INTEGER PRIMARY KEY NOT NULL,
-	fundo NUMERIC NOT NULL,
-	data_abertura DATETIME NOT NULL,
+	data_abertura DATETIME NOT NULL DEFAULT (datetime()),
 	data_fechamento DATETIME,
 	estoque_id INTEGER NOT NULL,
-	FOREIGN KEY (estoque_id) REFERENCES estoque (id) ON UPDATE CASCADE
+	FOREIGN KEY (estoque_id) REFERENCES estoque (id) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS caixa_fundo(
+	id INTEGER PRIMARY KEY NOT NULL,
+	valor NUMERIC NOT NULL,
+	data_alteracao DATETIME NOT NULL DEFAULT (datetime()),
+	caixa_id INTEGER NOT NULL,
+	FOREIGN KEY (caixa_id) REFERENCES caixa (id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS caixa_item(
 	id INTEGER PRIMARY KEY NOT NULL,
 	qtd INTEGER NOT NULL,
+	data_alteracao DATETIME NOT NULL DEFAULT (datetime()),
 	item_id INTEGER NOT NULL,
 	caixa_id INTEGER NOT NULL,
-	FOREIGN KEY (item_id) REFERENCES item (id) ON UPDATE CASCADE,
-	FOREIGN KEY (caixa_id) REFERENCES caixa (id) ON UPDATE CASCADE
+	FOREIGN KEY (item_id) REFERENCES item (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	FOREIGN KEY (caixa_id) REFERENCES caixa (id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS venda(
 	id INTEGER PRIMARY KEY NOT NULL,
 	valor_pago NUMERIC NOT NULL,
 	valor_venda NUMERIC NOT NULL,
-	data_pagamento DATETIME NOT NULL,
+	data_pagamento DATETIME NOT NULL DEFAULT (datetime()),
 	forma_pagamento_id INTEGER NOT NULL,
 	caixa_id INTEGER NOT NULL,
-	FOREIGN KEY (forma_pagamento_id) REFERENCES forma_pagamento (id) ON UPDATE CASCADE,
-	FOREIGN KEY (caixa_id) REFERENCES caixa (id) ON UPDATE CASCADE
+	FOREIGN KEY (forma_pagamento_id) REFERENCES forma_pagamento (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	FOREIGN KEY (caixa_id) REFERENCES caixa (id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS venda_item(
@@ -112,65 +117,159 @@ CREATE TABLE IF NOT EXISTS venda_item(
 	preco_venda NUMERIC,
 	venda_id INTEGER NOT NULL,
 	caixa_item_id INTEGER NOT NULL,
-	FOREIGN KEY (venda_id) REFERENCES venda (id) ON UPDATE CASCADE,
-	FOREIGN KEY (caixa_item_id) REFERENCES caixa_item (id) ON UPDATE CASCADE
+	FOREIGN KEY (venda_id) REFERENCES venda (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	FOREIGN KEY (caixa_item_id) REFERENCES caixa_item (id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS insumo(
-	id INTEGER PRIMARY KEY NOT NULL,
-	item_id INTEGER NOT NULL,
-	FOREIGN KEY (item_id) REFERENCES item (id) ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS insumo_item(
+CREATE TABLE IF NOT EXISTS formula_insumo(
 	id INTEGER PRIMARY KEY NOT NULL,
 	qtd_insumo_item INTEGER NOT NULL,
-	data_alteracao DATETIME NOT NULL,
+	ativo BOOLEAN NOT NULL DEFAULT true CHECK (ativo IN (false, true)),
+	data_alteracao DATETIME NOT NULL DEFAULT (datetime()),
 	insumo_id INTEGER NOT NULL,
 	item_id INTEGER NOT NULL,
-	FOREIGN KEY (insumo_id) REFERENCES insumo (id) ON UPDATE CASCADE,
-	FOREIGN KEY (item_id) REFERENCES item (id) ON UPDATE CASCADE
+	FOREIGN KEY (insumo_id) REFERENCES item (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	FOREIGN KEY (item_id) REFERENCES item (id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
+
 
 -- TRIGGERS ITEM
 
-CREATE TRIGGER IF NOT EXISTS insert_item
+CREATE TRIGGER IF NOT EXISTS validate_atual_insert_item
 	BEFORE INSERT
 	ON item
-	WHEN EXISTS(SELECT 1 FROM item WHERE nome = NEW.nome AND NEW.status_item_id > 0 LIMIT 1)
+	WHEN NEW.atual = false
 	BEGIN
-		SELECT RAISE(ROLLBACK, 'Item com mesmo "nome" ja existe!');
+		SELECT RAISE(ROLLBACK, 'Não é possível dar INSERT em "item" não "atual"!');
+	END;
+
+CREATE TRIGGER IF NOT EXISTS validate_nome_insert_item
+	BEFORE INSERT
+	ON item
+	WHEN EXISTS(SELECT 1 FROM item WHERE nome = NEW.nome AND NEW.atual = true LIMIT 1)
+	BEGIN
+		SELECT RAISE(ROLLBACK, '"item" com mesmo "nome" já existe!');
+	END;
+
+CREATE TRIGGER IF NOT EXISTS validate_atual_update_item
+	BEFORE UPDATE
+	ON item
+	WHEN OLD.atual = false
+	BEGIN
+		SELECT RAISE(ROLLBACK, 'Não é possível dar UPDATE em "item" não "atual"!');
 	END;
 
 CREATE TRIGGER IF NOT EXISTS update_item
 	BEFORE UPDATE
 	ON item
+	WHEN OLD.atual = true
 	BEGIN
-		INSERT INTO item (nome, qtd, custo, preco, data_alteracao, item_antes_id, status_item_id, unidade_medida_id)
-			VALUES (NEW.nome, NEW.qtd, NEW.custo, NEW.preco, NEW.data_alteracao, OLD.id, NEW.status_item_id, NEW.unidade_medida_id);
-		
 		UPDATE item
-		SET status_item_id = -OLD.status_item_id
+		SET atual = false
 		WHERE id = OLD.id;
+	
+		INSERT INTO item (nome, qtd, custo, preco, item_antes_id, ativo, unidade_medida_id)
+			VALUES (NEW.nome, NEW.qtd, NEW.custo, NEW.preco, OLD.id, NEW.ativo, NEW.unidade_medida_id);	
 		SELECT RAISE(IGNORE);
 	END;
 
-CREATE TRIGGER IF NOT EXISTS delete_item
+CREATE TRIGGER IF NOT EXISTS validate_delete_item
 	BEFORE DELETE
 	ON item
 	BEGIN
-		SELECT RAISE(ROLLBACK, 'Item nao pode ser deletado, altere "status_item_id" para seu inverso!');
+		SELECT RAISE(ROLLBACK, '"item" nao pode ser deletado, desative-o trocando "ativo" para "false"!');
 	END;
+
+
+-- TRIGGER CAIXA_ITEM
+
+-- novas_inserções_no_mesmo_id_atualizam_o_item_por_padrão
+CREATE TRIGGER IF NOT EXISTS update_caixa_item
+	BEFORE UPDATE
+	ON caixa_item
+	WHEN OLD.atual = true
+	BEGIN
+		INSERT INTO caixa_item (qtd, item_id, caixa_id)
+			VALUES (NEW.qtd, OLD.item_id, OLD.caixa_id);
+		
+		INSERT INTO caixa_fundo (valor, caixa_item)
+			SELECT (valor, caixa_item) FROM caixa_fundo AS cf
+				WHERE cf.data_alteracao = (SELECT MAX(data_alteracao) FROM caixa_fundo);
+				
+		SELECT RAISE(IGNORE);
+	END;
+
+CREATE TRIGGER IF NOT EXISTS validate_delete_caixa_item
+	BEFORE DELETE
+	ON caixa_item
+	BEGIN
+		SELECT RAISE(ROLLBACK, '"caixa_item" nao pode ser deletado, desative-o trocando "ativo" para "false"!');
+	END;
+
+
+-- TRIGGER ASSOCIADOS A CAIXA
+
+CREATE TRIGGER IF NOT EXISTS validate_caixa_insert_item
+	BEFORE INSERT
+	ON item
+	WHEN EXISTS(SELECT 1 FROM caixa WHERE data_fechamento = NULL LIMIT 1)
+	BEGIN
+		SELECT RAISE(ROLLBACK, 'Não é possível dar INSERT ou UPDATE em "item" com um "caixa" aberto!');
+	END;
+
+CREATE TRIGGER IF NOT EXISTS validate_caixa_insert_caixa_item
+	BEFORE INSERT
+	ON caixa_item
+	WHEN EXISTS(SELECT 1 FROM caixa WHERE NEW.caixa_id = id AND data_fechamento <> NULL LIMIT 1)
+	BEGIN
+		SELECT RAISE(ROLLBACK, 'Não é possível dar INSERT ou UPDATE em "caixa_item" com um "caixa" fechado!');
+	END;
+
+CREATE TRIGGER IF NOT EXISTS validate_caixa_insert_caixa_fundo
+	BEFORE INSERT
+	ON caixa_fundo
+	WHEN EXISTS(SELECT 1 FROM caixa WHERE NEW.caixa_id = id AND data_fechamento <> NULL LIMIT 1)
+	BEGIN
+		SELECT RAISE(ROLLBACK, 'Não é possível dar INSERT ou UPDATE em "caixa_fundo" com um "caixa" fechado!');
+	END;
+
 
 -- TRIGGER CAIXA
 
-CREATE TRIGGER IF NOT EXISTS insert_caixa
+CREATE TRIGGER IF NOT EXISTS validate_aberto_insert_caixa
 	BEFORE INSERT
 	ON caixa
 	WHEN EXISTS(SELECT 1 FROM caixa WHERE data_fechamento = NULL LIMIT 1)
 	BEGIN
-		SELECT RAISE(ROLLBACK, 'Ainda ha um caixa aberto!');
+		SELECT RAISE(ROLLBACK, 'Ainda há um "caixa" aberto!');
 	END;
+
+CREATE TRIGGER IF NOT EXISTS insert_caixa
+	BEFORE INSERT
+	ON caixa
+	WHEN NOT EXISTS(SELECT 1 FROM caixa WHERE data_fechamento = NULL LIMIT 1)
+	BEGIN
+		INSERT INTO estoque (data_alteracao) VALUES (datetime());
+		INSERT INTO estoque_item (estoque_id, item_id)
+			SELECT e.id, i.id
+				FROM estoque AS e, item AS i
+				WHERE
+					e.data_alteracao = (SELECT MAX(data_alteracao) FROM estoque)
+						AND
+					i.atual = true;
+	END;
+
+CREATE TRIGGER IF NOT EXISTS validate_update_caixa
+	BEFORE UPDATE
+	ON caixa
+	WHEN	OLD.id <> NEW.id
+		OR	OLD.data_abertura <> NEW.data_abertura
+		OR	OLD.estoque_id <> NEW.estoque_id
+		OR	OLD.data_fechamento <> NULL
+	BEGIN
+		SELECT RAISE(ROLLBACK, 'UPDATE em "caixa" desautorizado! Você tentou mudar "id", "data_abertura", "estoque_id" ou alterar um "caixa" já fechado.');
+	END;
+
 
 -- VIEWS COM REDUNDANCIAS
 
@@ -180,15 +279,14 @@ CREATE VIEW IF NOT EXISTS item_view (
 	qtd_item,
 	custo_item,
 	preco_item,
+	ativo,
+	atual,
 	data_alteracao,
-	status,
 	unidade_medida
 ) AS
-	SELECT  i.id, i.nome, i.qtd, i.custo, i.preco, i.data_alteracao,
-	si.nome,
+	SELECT  i.id, i.nome, i.qtd, i.custo, i.preco, i.ativo, i.atual, i.data_alteracao,
 	um.nome
 	FROM item AS i
-	INNER JOIN status_item AS si ON si.id = i.status_item_id 
 	INNER JOIN unidade_medida AS um ON um.id = i.unidade_medida_id;
 
 CREATE VIEW IF NOT EXISTS caixa_item_view (
@@ -196,9 +294,9 @@ CREATE VIEW IF NOT EXISTS caixa_item_view (
 	item_id,
 	nome_item,
 	qtd_caixa_item,
+	data_alteracao,
 	custo_item,
 	preco_item,
-	status_item,
 	unidade_medida,
 	caixa_id,
 	data_abertura,
@@ -207,8 +305,8 @@ CREATE VIEW IF NOT EXISTS caixa_item_view (
 ) AS
 	SELECT ci.id,
 			iv.id, iv.nome,
-			ci.qtd,
-			iv.custo_item, iv.preco_item, iv.status, iv.unidade_medida,
+			ci.qtd, ci.data_alteracao,
+			iv.custo_item, iv.preco_item, iv.unidade_medida,
 			c.id, c.data_abertura,
 			e.id, e.data_alteracao
 	FROM caixa_item AS ci
@@ -225,7 +323,6 @@ CREATE VIEW IF NOT EXISTS venda_item_view (
 	preco_venda_item,
 	custo_item,
 	preco_item,
-	status_item,
 	unidade_medida,
 	desconto_venda_item,
 	venda_id,
@@ -233,13 +330,14 @@ CREATE VIEW IF NOT EXISTS venda_item_view (
 ) AS
 	SELECT vi.id,
 	civ.item_id, civ.id, civ.nome_item,
-	vi.qtd, vi.preco_venda,
-	civ.custo_item, civ.preco_item, civ.status_item, civ.unidade_medida,
+	vi.qtd,	vi.preco_venda,
+	civ.custo_item, civ.preco_item, civ.unidade_medida,
 	civ.preco_item-vi.preco_venda,
 	v.id, v.data_pagamento
 	FROM venda_item AS vi
 	INNER JOIN caixa_item_view AS civ ON civ.id = vi.caixa_item_id
 	INNER JOIN venda AS v ON v.id = vi.venda_id;
+
 
 -- VIEWSS DE ULTIMOS
 
@@ -247,24 +345,21 @@ CREATE VIEW IF NOT EXISTS item_atual
 AS
 	SELECT iv.* FROM item_view AS iv
 	INNER JOIN item AS i ON i.id = iv.id
-	WHERE i.status_item_id > 0;
+	WHERE i.atual = true;
+
+CREATE VIEW IF NOT EXISTS caixa_item_atual 
+AS
+	SELECT * FROM caixa_item_view AS civ
+	WHERE civ.data_alteracao = (SELECT MAX(data_alteracao) FROM caixa_item WHERE civ.item_id = item_id);
 
 CREATE VIEW IF NOT EXISTS caixa_item_aberto
 AS
-	SELECT civ.* FROM caixa_item_view AS civ
-	INNER JOIN caixa AS c ON c.id = civ.caixa_id
+	SELECT cia.* FROM caixa_item_atual AS cia
+	INNER JOIN caixa AS c ON c.id = cia.caixa_id
 	WHERE c.data_fechamento = NULL;
 
--- INSERTS BASICOS
 
-INSERT INTO status_item (id, nome)
-	VALUES	(-3, 'APENAS_INSUMO_ANTERIOR'),
-			(-2, 'INATIVO_ANTERIOR'),
-			(-1, 'ATIVO_ANTERIOR'),
-			(0, 'INDEFINIDO'),
-			(1, 'ATIVO'),
-			(2, 'INATIVO'),
-			(3, 'APENAS_INSUMO');
+-- INSERTS BASICOS
 
 INSERT INTO unidade_medida (nome)
 	VALUES	('UNIDADES'),
@@ -281,4 +376,3 @@ INSERT INTO forma_pagamento (nome)
 			('PIX'),
 			('CARTAO_DEBITO'),
 			('CARTAO_CREDITO');
-
