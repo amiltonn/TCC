8b66c0dfa7d64db41645d3294664214ae662387b
Gabriel <gabrielsardinhaferreira@gmail.com>
2022-05-15 00:50:06 -0300

    criando rotina de preencher objeto

--- app/src/main/java/com/tcc/zipzop/database/ZipZopMigrations.java
+++ app/src/main/java/com/tcc/zipzop/database/ZipZopMigrations.java
@@ -1,5 +1,19 @@
 package com.tcc.zipzop.database;
 
+import androidx.annotation.NonNull;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 public class ZipZopMigrations {
+
+    private static final Migration MIGRATION_UNICA = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("");
+        }
+    };
+
+
+    static final Migration[] TODAS_MIGRATIONS = {MIGRATION_UNICA};
 
 }
