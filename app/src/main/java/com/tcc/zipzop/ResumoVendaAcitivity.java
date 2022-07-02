906ddd8d0bf1e116476e558bb100a1a04f753a24
amilton.jesus <amilton.jesus@semprecub.com>
2022-07-02 18:36:53 -0300

    tabela de Relatorio

--- app/src/main/java/com/tcc/zipzop/ResumoVendaAcitivity.java
+++ app/src/main/java/com/tcc/zipzop/ResumoVendaAcitivity.java
@@ -1,17 +1,22 @@
 package com.tcc.zipzop;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ListView;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
-import com.tcc.zipzop.adapter.ProdutoVendaAdapterActivity;
 import com.tcc.zipzop.typeconverter.ObjectWrapperForBinder;
 import com.tcc.zipzop.view.VendaProdutoView;
 import com.tcc.zipzop.view.VendaView;
 
 import java.util.List;
+@RequiresApi(api = Build.VERSION_CODES.N)
 
 public class ResumoVendaAcitivity extends AppCompatActivity {
     private VendaView vendaView;
@@ -28,15 +33,34 @@ public class ResumoVendaAcitivity extends AppCompatActivity {
         vendaView= (VendaView) vendaViewReceived;
         Log.d("Resumo", String.valueOf(vendaView));
 
-        resumoProdutoVendaListView();
+        resumoProdutoVendatable();
 
 
     }
 
-    private void resumoProdutoVendaListView() {
-        ListView resumoVendaProdutoList = findViewById(R.id.resumoVendaProdutoList);
+
+    private void resumoProdutoVendatable() {
         vendaProdutoViewList = vendaView.getVendaProdutoViewList();
-        ProdutoVendaAdapterActivity produtoVendaAdapterActivity = new ProdutoVendaAdapterActivity(this,vendaProdutoViewList);
-        resumoVendaProdutoList.setAdapter(produtoVendaAdapterActivity);
+        TableLayout tableLayoutVendaProduto = findViewById(R.id.tableLayoutVendaProduto);
+        vendaProdutoViewList.forEach(vPVenda ->{
+            String [] visualizacaoVendaProduto = {vPVenda.getCaixaProdutoView().getProdutoView().getProduto().getNome(),
+            String.valueOf(vPVenda.getCaixaProdutoView().getProdutoView().getProduto().getPreco()),
+                    String.valueOf(vPVenda.getVendaProduto().getQtd()),
+                    String.valueOf(vPVenda.getVendaProduto().getPrecoVenda())};
+            Log.d("VIZUALIZAÇAO", String.valueOf(visualizacaoVendaProduto));
+            TableRow row = new TableRow(getBaseContext());
+            TextView textView = null;
+            for (int i=0;i<4;i++) {
+                textView = new TextView(getBaseContext());
+                textView.setGravity(Gravity.CENTER);
+                textView.setPadding(10, 10, 10, 10);
+                textView.setBackgroundResource(R.color.botao);
+                textView.setText(visualizacaoVendaProduto[i]);
+                textView.setTextColor(getResources().getColor(R.color.letra));
+                row.addView(textView);
+            }
+            tableLayoutVendaProduto.addView(row);
+        });
+
     }
 }
