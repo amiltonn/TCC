296ace09152148b949158bffc969f54bb1c2434d
keybot <mail.keybot@gmail.com>
2022-07-02 13:17:40 -0300

    habilitando e desabilitando botões conforme presença de caixa aberto

--- app/src/main/java/com/tcc/zipzop/VendaActivity.java
+++ app/src/main/java/com/tcc/zipzop/VendaActivity.java
@@ -1,15 +1,21 @@
 package com.tcc.zipzop;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.tcc.zipzop.asynctask.caixa.ChecarCaixaAbertoTask;
+import com.tcc.zipzop.typeconverter.ObjectWrapperForBinder;
+
+import java.util.concurrent.ExecutionException;
+
 public class VendaActivity extends AppCompatActivity {
-    private Button ButtonNovaVenda;
+    private Button buttonNovaVenda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +26,12 @@ public class VendaActivity extends AppCompatActivity {
 
 
         //Função do botão para ir para tela de nova venda
-        ButtonNovaVenda = findViewById(R.id.bt_NovaVenda);
-        ButtonNovaVenda.setOnClickListener(new View.OnClickListener() {
+        buttonNovaVenda = findViewById(R.id.bt_NovaVenda);
+        buttonNovaVenda.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(VendaActivity.this,NovaVendaActivity.class);
-
+                Intent intent = new Intent(VendaActivity.this, NovaVendaActivity.class);
                 startActivity(intent);
 
             }
