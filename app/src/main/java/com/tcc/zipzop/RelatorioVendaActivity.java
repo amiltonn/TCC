1672301ca323462fc082d454334a02f7478aa9a4
keybot <mail.keybot@gmail.com>
2022-07-04 04:12:25 -0300

    listando relatorio de vendas

---
+++ app/src/main/java/com/tcc/zipzop/RelatorioVendaActivity.java
@@ -0,0 +1,14 @@
+package com.tcc.zipzop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelatorioVendaActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_relatorio_venda);
+  }
+}
